13fb54d0f2670df0d9b42c99f3057896a2ce13ca
david-ruane <159771807+david-ruane@users.noreply.github.com>
2024-02-12 10:21:24 +0000

    Update naptan.src.js

--- js/naptan.src.js
+++ js/naptan.src.js
@@ -1,118 +1,69 @@
-const colors = [
-    '#42d4f4',
-    '#f032e6',
-    '#ffe119',
-    '#f58231',
-    '#3cb44b',
-    '#e6194B',
-    '#4363d8',
-    '#fabed4'
-]
-
-const labels = [
-    'Marked Stop', 'Custom & Practice Stop', 'Bus Station Entrance', 'Bus Station Bay', 
-    'Railway Station', 'Taxi Rank', 'Metro Platform', 'Ferry Terminal'];
-
-function getColor(d) {
-    return d.BusStopType == 'MKD' ? colors[0] :
-        d.BusStopType == 'CUS' ? colors[1] :
-        d.StopType == 'BCE' ? colors[2] :
-        d.StopType == 'BCS' ? colors[3] :
-        d.StopType == 'RLY' ? colors[4] :
-        d.StopType == 'TXR' ? colors[5] :
-        d.StopType == 'PLT' ? colors[6] :
-        d.StopType == 'FTD' ? colors[7] :
-        'ffffff';
+const colors = ["#42d4f4", "#f032e6", "#ffe119", "#f58231", "#3cb44b", "#e6194B", "#4363d8", "#fabed4"],
+    labels = ["Marked Stop", "Custom & Practice Stop", "Bus Station Entrance", "Bus Station Bay", "Railway Station", "Taxi Rank", "Metro Platform", "Ferry Terminal"];
+function getColor(o) {
+    return "MKD" == o.BusStopType
+        ? colors[0]
+        : "CUS" == o.BusStopType
+        ? colors[1]
+        : "BCE" == o.StopType
+        ? colors[2]
+        : "BCS" == o.StopType
+        ? colors[3]
+        : "RLY" == o.StopType
+        ? colors[4]
+        : "TXR" == o.StopType
+        ? colors[5]
+        : "PLT" == o.StopType
+        ? colors[6]
+        : "FTD" == o.StopType
+        ? colors[7]
+        : "ffffff";
 }
-
-function naptanMarker(feature) {
-    return {
-        radius: 6,
-        fillColor: getColor(feature.properties),
-        color: '#ffffff',
-        weight: .5,
-        opacity: 1,
-        fillOpacity: 0.9
-    };
+function naptanMarker(o) {
+    return { radius: 6, fillColor: getColor(o.properties), color: "#ffffff", weight: 0.5, opacity: 1, fillOpacity: 0.9 };
 }
-
-function forEachFeature(feature, layer) {
-    var popupContent = '<table>';
-    for (var p in feature.properties) {
-        popupContent += '<tr><td><b>' + p + '</b></td><td>' + feature.properties[p] + '</td></tr>';
-    }
-    popupContent += '</table>';
-    layer.feature.properties.searchItem = layer.feature.properties.AtcoCode + ', '
-     + layer.feature.properties.CommonName;
-    layer.bindPopup(popupContent);
-};
-
-$.support.cors = true;
-
-var stopmarkers = L.markerClusterGroup({
-    MaxClusterRadius: 200,
-    disableClusteringAtZoom: 10,
-    spiderfyOnMaxZoom: false
-});
-
-var baseMaps = {
-    'OpenStreetMap': osm,
-};
-
-var osm = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://www.transportforireland.ie/">National Transport Authority</a> | &copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-});
-
-var map = L.map('map', {
-    center: [53.43, -7.95],
-    zoom: 7,
-    layers: [osm, stopmarkers]
-});
-
-$.getJSON( "js/naptan.json",
-    function(data) {
-    napLocations = L.geoJSON(data, {
+function forEachFeature(o, e) {
+    var r = "<table>";
+    for (var t in o.properties) r += "<tr><td><b>" + t + "</b></td><td>" + o.properties[t] + "</td></tr>";
+    (r += "</table>"), (e.feature.properties.searchItem = e.feature.properties.AtcoCode + ", " + e.feature.properties.CommonName), e.bindPopup(r);
+}
+$.support.cors = !0;
+var stopmarkers = L.markerClusterGroup({ MaxClusterRadius: 200, disableClusteringAtZoom: 10, spiderfyOnMaxZoom: !1 }),
+    baseMaps = { OpenStreetMap: osm },
+    osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+      }).addTo(map);
+      
+    map = L.map("map", { center: [53.43, -7.95], zoom: 7, layers: [osm, stopmarkers] });
+$.getJSON("js/naptan.json", function (o) {
+    (napLocations = L.geoJSON(o, {
         onEachFeature: forEachFeature,
-        pointToLayer: function(feature, latlng) {
-            return L.circleMarker(latlng, naptanMarker(feature));
-        }
-    });
-    stopmarkers.addLayer(napLocations);
+        pointToLayer: function (o, e) {
+            return L.circleMarker(e, naptanMarker(o));
+        },
+    })),
+        stopmarkers.addLayer(napLocations);
 });
-
 var searchControl = new L.control.search({
     layer: stopmarkers,
-    initial: false,
-    marker: false,
-    propertyName: 'searchItem',
-    textPlaceholder: 'Search...',
-    collapsed: false,
-    autoCollapse: true,
+    initial: !1,
+    marker: !1,
+    propertyName: "searchItem",
+    textPlaceholder: "Search...",
+    collapsed: !1,
+    autoCollapse: !0,
     zoom: 12,
-    moveToLocation: function(latlng, title, map) {
-            map.setView(latlng, 15); // access the zoom
-        }
-  }).addTo(map);
-
-searchControl.on('search:locationfound', function(e) {
-        e.layer.openPopup();
+    moveToLocation: function (o, e, r) {
+        r.setView(o, 15);
+    },
+}).addTo(map);
+searchControl.on("search:locationfound", function (o) {
+    o.layer.openPopup();
 });
-
-var legend = L.control({
-    position: 'bottomright'
-});
-
-legend.onAdd = function(map) {
-    var div = L.DomUtil.create('div', 'info legend');
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < colors.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + colors[i] + '"></i> ' + labels[i] + '<br>';
-    }
-
-    return div;
-};
-
-legend.addTo(map);
+var legend = L.control({ position: "bottomright" });
+(legend.onAdd = function (o) {
+    for (var e = L.DomUtil.create("div", "info legend"), r = 0; r < colors.length; r++) e.innerHTML += '<i style="background:' + colors[r] + '"></i> ' + labels[r] + "<br>";
+    return e;
+}),
+    legend.addTo(map);
