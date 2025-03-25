36686995e453234c1a2a143330a1585bb06a69d8
david-ruane <159771807+david-ruane@users.noreply.github.com>
2025-03-25 11:12:24 +0000

    Update naptan.src.js

--- js/naptan.src.js
+++ js/naptan.src.js
@@ -30,21 +30,17 @@ function forEachFeature(o, e) {
 $.support.cors = !0;
 var stopmarkers = L.markerClusterGroup({ MaxClusterRadius: 200, disableClusteringAtZoom: 10, spiderfyOnMaxZoom: !1 }),
     baseMaps = { OpenStreetMap: osm },
-    osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 20,
-        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-      });
-      
-    map = L.map("map", { center: [53.43, -7.95], zoom: 7, layers: [osm, stopmarkers] });
-$.getJSON("js/naptan.json", function (o) {
-    (napLocations = L.geoJSON(o, {
-        onEachFeature: forEachFeature,
-        pointToLayer: function (o, e) {
-            return L.circleMarker(e, naptanMarker(o));
-        },
-    })),
-        stopmarkers.addLayer(napLocations);
-});
+    osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", { maxZoom: 20, attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>' });
+(map = L.map("map", { center: [53.43, -7.95], zoom: 7, layers: [osm, stopmarkers] })),
+    $.getJSON("https://corsproxy.io/?url=https://www.transportforireland.ie/transitData/Data/naptan.json", function (o) {
+        (napLocations = L.geoJSON(o, {
+            onEachFeature: forEachFeature,
+            pointToLayer: function (o, e) {
+                return L.circleMarker(e, naptanMarker(o));
+            },
+        })),
+            stopmarkers.addLayer(napLocations);
+    });
 var searchControl = new L.control.search({
     layer: stopmarkers,
     initial: !1,
